7e3e247e98415e9b3f14cae19036bfda29dc0382
Ross Nelson <ross.nelson@gmail.com>
2025-01-10 09:46:41 -0600

    First pass at building the models

---
+++ src/Libexec.FetchReceiptProcessor/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Libexec.FetchReceiptProcessor.Models;
+
+public class Item
+{
+    [JsonRequired]
+    [JsonPropertyName("shortDescription")]
+    [Description("The Short Product Description for the item.")]
+    [RegularExpression(@"^[\\w\\s\\-]+$")]
+    public required string ShortDescription { get; set; }
+    
+    [JsonIgnore]
+    [Description("The total price paid for this item.")]
+    [RegularExpression(@"^\\d+\\.\\d{2}$")]
+    public required decimal Price { get; set; }
+    
+    [JsonRequired]
+    [JsonPropertyName("price")]
+    private string PriceString => Price.ToString("0.00");
+}
---
+++ src/Libexec.FetchReceiptProcessor/Models/Receipt.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Libexec.FetchReceiptProcessor.Models;
+
+public class Receipt
+{
+    [JsonRequired]
+    [JsonPropertyName("retailer")]
+    [Description("The name of the retailer or store the receipt is from.")]
+    [RegularExpression(@"^[\\w\\s\\-&]+$")]
+    public required string Retailer { get; set; }
+    
+    [JsonIgnore]
+    [Description("The date of the purchase printed on the receipt.")]
+    public required DateOnly PurchaseDate { get; set; }
+    
+    [JsonIgnore]
+    [Description("The time of the purchase printed on the receipt. 24-hour time expected.")]
+    public required DateOnly PurchaseTime { get; set; }
+    
+    [JsonRequired]
+    [JsonPropertyName("items")]
+    [Description("The list of items purchased on the receipt.")]
+    [MinLength(1)]
+    public List<Item> Items { get; private set; } = [];
+    
+    [JsonIgnore]
+    [Description("The total amount paid on the receipt.")]
+    [RegularExpression(@"^\\d+\\.\\d{2}$")]
+    public required decimal Total { get; set; }
+    
+    [JsonRequired]
+    [JsonPropertyName("purchaseDate")]
+    private string PurchaseDateString => PurchaseDate.ToString("yyyy-MM-dd");
+    
+    [JsonRequired]
+    [JsonPropertyName("purchaseTime")]
+    private string PurchaseTimeString => PurchaseTime.ToString("HH:mm");
+    
+    [JsonRequired]
+    [JsonPropertyName("total")]
+    private string TotalString => Total.ToString("0.00");
+}
